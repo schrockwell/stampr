d03badbd3246b54704a04f04bc41b84bb70d3bc4
Rockwell Schrock <rockwell@schrock.me>
2022-06-27 23:32:16 -0400

    UI nonsense

--- lib/stampr_web/live/episode_live.ex
+++ lib/stampr_web/live/episode_live.ex
@@ -18,15 +18,27 @@ defmodule StamprWeb.EpisodeLive do
 
   def handle_event("start", _, socket) do
     episode = Stampr.start_episode(socket.assigns.episode)
-
     {:noreply, assign_episode(socket, episode: episode)}
   end
 
   def handle_event("add-marker", params, socket) do
     name = params["name"] || "Marker"
     episode = Stampr.add_episode_marker(socket.assigns.episode, %{name: name})
-
     {:noreply, assign_episode(socket, episode: episode)}
+  end
+
+  def handle_event("keydown", %{"key" => "m"}, socket) do
+    episode = Stampr.add_episode_marker(socket.assigns.episode, %{name: "Marker"})
+    {:noreply, assign_episode(socket, episode: episode)}
+  end
+
+  def handle_event("keydown", %{"key" => "t"}, socket) do
+    episode = Stampr.add_episode_marker(socket.assigns.episode, %{name: "Transition"})
+    {:noreply, assign_episode(socket, episode: episode)}
+  end
+
+  def handle_event("keydown", _, socket) do
+    {:noreply, socket}
   end
 
   def handle_info({_, :episode_updated}, socket) do
@@ -45,7 +57,7 @@ defmodule StamprWeb.EpisodeLive do
 
     socket
     |> assign(:episode, episode)
-    |> assign(:markers, Enum.sort_by(episode.markers, & &1.at))
+    |> assign(:markers, Enum.sort_by(episode.markers, &(-&1.at)))
     |> assign(:started?, !!episode.started_at)
   end
 
@@ -53,12 +65,6 @@ defmodule StamprWeb.EpisodeLive do
     assign(socket, :now, System.monotonic_time(:millisecond))
   end
 
-
-  defp elapsed(%{started_at: nil} = episode, _now) do
-    ms_to_hms(0)
-  end
-
-  defp elapsed(episode, now) do
-    ms_to_hms(now - episode.started_at)
-  end
+  defp elapsed(%{started_at: nil}, _now), do: ms_to_hms(0)
+  defp elapsed(episode, now), do: ms_to_hms(now - episode.started_at)
 end
